9546d2df3692de18bd8bfce23249bef708517c21
tarun-c-duggempudi <tarun.mail09@gmail.com>
2025-04-19 12:45:41 +1000

    add project structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+function Home() {
+
+  return (
+    <main className="bg-primary px-4 pb-8 max-w-[400px] mx-auto">
+        <div className="container bg-white ">
+            <h1 className="text-4xl font-bold text-primary">TEST</h1>
+        </div>
+    </main>
+  )
+}
+
+export default Home
